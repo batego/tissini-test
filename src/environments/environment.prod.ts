bd8e0e67093c1d2f665a6825c6b41777fe4df0d4
Boris Terraza G <ingterraza@gmail.com>
2021-08-30 16:37:30 -0500

    Agregar variables de entorno

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  urlBase: 'https://v3.tissini.app/api/v3/',
+  files: 'https://v3.tissini.app/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   urlBase: 'https://v3.tissini.app/api/v3/',
   files: 'https://v3.tissini.app/'
 };
